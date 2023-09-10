3670fee6b3a1b66cf7f60bbb4e55a87d63ab590e
thetheu122 <math.santosmed@gmail.com>
2023-09-10 13:45:12 -0300

    card do carrinho feito

--- frontend/src/components/user/carrinhoCard/index.js
+++ frontend/src/components/user/carrinhoCard/index.js
@@ -1,10 +1,10 @@
 import './index.scss'
 import '../../../assets/config/fonts-config.scss'
 
-import Carrinho from '../../../assets/images/icons/shopping-cart_icon.svg'
 import Star from '../../../assets/images/icons/star_icon.svg'
+import Coracao from '../../../assets/images/icons/coracao_icon.svg'
 
-export default function CardProduct() {
+export default function CardCarrinho() {
 
 
     return (
@@ -12,18 +12,21 @@ export default function CardProduct() {
 
             <div className='alimento'></div>
 
-            <div className='pequenasInformações'>
+            <div className='pequenasInformacoes'>
 
                 <div className='descricaoProduto'>
                     <h3>Pizza Margherita</h3>
-                    <div className='pretin'>
-                        <p>4.9</p>
-                        <img alt='estrela' src={Star} />
+                    <div className='circulo'>
+                        <img alt='coracao' src={Coracao}/>
                     </div>
                 </div>
 
-                <div className='circulo'>
-                    <img alt='carrinho' src={Carrinho} />
+                <div className='baixoDescricaoProdutos'>
+                    <div className='pretin'>
+                        <p>4.9</p>
+                        <img alt='estrela' src={Star}/>
+                    </div>
+                    <p>R$ 80,00</p>
                 </div>
 
 
--- frontend/src/pages/app/App.js
+++ frontend/src/pages/app/App.js
@@ -2,13 +2,14 @@ import Cabecalho from '../../components/user/cabecalho';
 import CardProduto from '../../components/user/card-produto';
 import CardProduct from '../../components/user/cardProduct';
 import Carrinho from '../../components/user/carrinho';
+import CardCarrinho from '../../components/user/carrinhoCard';
 import Rodape from '../../components/user/rodape';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <CardProduct/> 
+      <CardCarrinho/> 
     </div>
   );
 }
