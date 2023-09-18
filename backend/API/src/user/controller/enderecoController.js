8ac8eaec0ee8cf9edaab401af9bb005c65781a75
MatheusMedeiros <129508325+thetheu122@users.noreply.github.com>
2023-09-18 18:58:31 -0300

    tratamento de erro no login e cadastro do cliente

--- backend/API/src/user/controller/enderecoController.js
+++ backend/API/src/user/controller/enderecoController.js
@@ -7,6 +7,34 @@ import { cadastrarEndereco } from "../repository/enderecoRepository.js";
 
 server.post('/endereco/cadastro', async (req, resp) => {
     let resposta = req.body
+
+    {/*if (!resposta) {
+        throw new Error("Os dados do cadastro do endereço está faltando");
+    }
+
+    const camposObrigatorios = [
+        "endereco",
+        "cartao",
+        "cliente",
+        "email",
+        "telefone",
+        "senha",
+        "cpf",
+        "nascimento"
+    ];
+
+    const camposFaltando = [];
+
+    camposObrigatorios.forEach((campo) => {
+        if (!(campo in resposta) || !resposta[campo]) {
+            camposFaltando.push(campo);
+        }
+    });
+
+    if (camposFaltando.length > 0) {
+        const mensagemErro = `Campos obrigatórios faltando: ${camposFaltando.join(", ")}`;
+        throw new Error(mensagemErro);
+    }*/}
     let respe = await cadastrarEndereco(resposta)
     resp.send(respe)
 })
