7529ac954b277c19182c6f6c9c18d3adf632add7
thetheu122 <math.santosmed@gmail.com>
2023-10-17 11:42:58 -0300

    editar conta continuado

--- frontend/src/components/perfil/myAccount/index.js
+++ frontend/src/components/perfil/myAccount/index.js
@@ -2,9 +2,15 @@ import './index.scss'
 import Logo from '../../../assets/images/logo.svg'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+
+import Aberto from '../../../assets/images/pictures/olho-aberto.svg'
+import Fechado from '../../../assets/images/pictures/olho-fechado.svg'
+import { toast } from 'react-toastify'
+
 export default function MyAccount() {
 
     //DADOS DO CLIENTE
+    const [id, setiD] = useState(0)
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
     const [telefone, setTelefone] = useState('')
@@ -23,13 +29,16 @@ export default function MyAccount() {
     //CONTROLADOR DE EDIÇÃO
     const [edt, setEdt] = useState(false)
 
+    //CONTROLADOR DE SENHA
+    const [showPassword, setShowPassword] = useState(false)
+
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 let usuario = localStorage.getItem('usuario-logado');
                 usuario = JSON.parse(usuario);
-                console.log(usuario.id)
+                setiD(usuario.id)
                 let rep = await axios.get(`http://localhost:5000/cliente/info?id=${usuario.id}`);
                 let response = rep.data
                 console.log(response)
@@ -40,8 +49,8 @@ export default function MyAccount() {
                 const nomeCompleto = response.cliente;
                 const partesNome = nomeCompleto.split(' ');
 
-                const n = partesNome[0]; 
-                const s = partesNome.slice(1).join(' '); 
+                const n = partesNome[0];
+                const s = partesNome.slice(1).join(' ');
                 setNome(n)
                 setSobrenome(s)
 
@@ -50,13 +59,14 @@ export default function MyAccount() {
                 const data = response.dtnascimento
                 const numeros = data.split('/');
 
+
                 const d = parseInt(numeros[0], 10);
                 const m = parseInt(numeros[1], 10);
                 const a = parseInt(numeros[2], 10);
                 setDia(d)
                 setMes(m)
                 setAno(a)
-                
+
                 setEmail(response.email)
                 setUf(response.estado)
                 setNum(response.numero)
@@ -71,6 +81,18 @@ export default function MyAccount() {
         fetchData();
     }, []);
 
+    const alterarUser = async () => {
+        try {
+            
+
+
+        } catch (error) {
+            toast.error(error)
+            console.log(error)
+        }
+    };
+
+
     return (
         <div className="minhaConta">
             <img className='logoCorleone' src={Logo} />
@@ -79,40 +101,44 @@ export default function MyAccount() {
             <div className='baixoMinhaConta'>
                 <div className='esquerdaMinhaConta'>
                     <p>Conta</p>
-                    <input placeholder='Email' value={email} />
+                    <input placeholder='Email' value={email} disabled={!edt} onChange={(e) => setEmail(e.target.value)} />
 
                     <div className='senhaTelefone'>
-                        <input placeholder='Senha' type='password' value={senha} />
-                        <input placeholder='Telefone' value={telefone} />
+                        <div className='senha'>
+                            <input className='in' disabled={!edt} placeholder='Senha' type={showPassword ? 'text' : 'password'} value={senha} onChange={(e) => setSenha(e.target.value)} />
+                            {showPassword ? <img src={Aberto} style={{ cursor: 'pointer' }} onClick={() => setShowPassword(false)}/> : <img src={Fechado} style={{ cursor: 'pointer' }} onClick={() => setShowPassword(true)}/>}
+                        </div>
+
+                        <input placeholder='Telefone' value={telefone} disabled={!edt} onChange={(e) => setTelefone(e.target.value)} />
                     </div>
 
                     <p>Dados Pessoais</p>
-                    <input placeholder='Nome' value={nome} />
-                    <input placeholder='Sobrenome' value={sobrenome} />
+                    <input placeholder='Nome' value={nome} disabled={!edt} onChange={(e) => setNome(e.target.value)} />
+                    <input placeholder='Sobrenome' value={sobrenome} disabled={!edt} onChange={(e => setSobrenome(e.target.value))} />
                     <div className='dataNascimento' >
-                        <input placeholder='Dia' value={dia} />
-                        <input placeholder='Mês' value={mes} />
-                        <input placeholder='Ano' value={ano} />
+                        <input placeholder='Dia' value={dia} disabled={!edt} onChange={(e => setDia(e.target.value))} />
+                        <input placeholder='Mês' value={mes} disabled={!edt} onChange={(e => setMes(e.target.value))} />
+                        <input placeholder='Ano' value={ano} disabled={!edt} onChange={(e => setAno(e.target.value))} />
                     </div>
                 </div>
 
                 <div className='direitaMinhaConta'>
-                    <input className='cpfzin' placeholder='CPF' value={cpf} />
+                    <input className='cpfzin' placeholder='CPF' value={cpf} disabled={!edt} onChange={(e => setCpf(e.target.value))} />
 
                     <p>Logradouro</p>
-                    <input placeholder='UF' value={uf} />
-                    <input placeholder='Cidade' value={cidade} />
-                    <input placeholder='Bairro' value={bairro} />
+                    <input placeholder='UF' value={uf} disabled={!edt} onChange={(e => setUf(e.target.value))} />
+                    <input placeholder='Cidade' value={cidade} disabled={!edt} onChange={(e => setCidade(e.target.value))} />
+                    <input placeholder='Bairro' value={bairro} disabled={!edt} onChange={(e => setBairro(e.target.value))} />
                     <div className='metadinha'>
-                        <input placeholder='Num.' value={num} />
-                        <input placeholder='CEP' value={cep} />
+                        <input placeholder='Num.' value={num} disabled={!edt} onChange={(e => setNum(e.target.value))} />
+                        <input placeholder='CEP' value={cep} disabled={!edt} onChange={(e => setCep(e.target.value))} />
                     </div>
 
                 </div>
 
             </div>
             <div className='duplada'>
-                <button className='butaum'>Editar</button>
+                <button className='butaum' onClick={() => setEdt(!edt)}>Editar</button>
                 <button className='butaumm'>Método de Pagamento</button>
             </div>
         </div>
--- frontend/src/pages/cardapio/index.js
+++ frontend/src/pages/cardapio/index.js
@@ -141,18 +141,18 @@ export default function Cardapio() {
     }, [pesquisa, vegano, vegetariano, intoleranteagluten, intoleranteaovo, intolerantealactose])
 
     const altPag = (num) => {
-        if(num == 'ant'){
+        if (num == 'ant') {
             setPaginaAtual(paginaAtual - 1)
             window.scrollTo({
                 top: 400,
                 behavior: 'smooth',
-              })
+            })
         }
-        else if(num == 'pro'){
+        else if (num == 'pro') {
             setPaginaAtual(paginaAtual + 1)
             window.scrollTo({ top: 0, behavior: 'smooth' });
         }
-        else{
+        else {
             setPaginaAtual(num)
             window.scrollTo({ top: 0, behavior: 'smooth' });
         }
@@ -161,21 +161,17 @@ export default function Cardapio() {
         <main className='cardapio'>
             <Cabecalho />
             <div className='cima'>
-                <h1>PIZZAS</h1>
-                <div className='opcoes-cima'>
-                    <h2>Bebidas</h2>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="2" height="35" viewBox="0 0 3 35" fill="none">
-                        <path d="M1.5 1.5V33.5" stroke="white" stroke-width="2" stroke-linecap="round" />
-                    </svg>
-                    <h2>Sobremesas</h2>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="2" height="35" viewBox="0 0 3 35" fill="none">
-                        <path d="M1.5 1.5V33.5" stroke="white" stroke-width="2" stroke-linecap="round" />
-                    </svg>
-                    <h2>Vegetarianas</h2>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="2" height="35" viewBox="0 0 3 35" fill="none">
-                        <path d="M1.5 1.5V33.5" stroke="white" stroke-width="2" stroke-linecap="round" />
-                    </svg>
-                    <h2>Favoritos</h2>
+                <div className='fpizza'>
+                    <h1>Pizzas</h1>
+                </div>
+                <div className='fsobremesas'>
+                    <h1>Sobremesas</h1>
+                </div>
+                <div className='fbebida'>
+                    <h1>Bebidas</h1>
+                </div>
+                <div className='fvegetariana'>
+                    <h1>Vegetarianas</h1>
                 </div>
             </div>
 
@@ -258,7 +254,7 @@ export default function Cardapio() {
                                         preco: item.preço,
                                         imagem: item.imagem,
                                         id: item.ID,
-                                        media:item.media
+                                        media: item.media
                                     }}
 
 
@@ -305,7 +301,7 @@ export default function Cardapio() {
                                             <path d="M1.5 9.5H23M23 9.5L13.561 1.5M23 9.5L13.561 17.5" stroke="#53220D" stroke-width="2" stroke-linecap="round" />
                                         </svg>
                                     </button>
-                                ): (
+                                ) : (
                                     <button className='negado'>
                                         <p>Próximo</p>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="19" viewBox="0 0 25 19" fill="none">
