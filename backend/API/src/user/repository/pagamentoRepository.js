21accc12fe2bf39261318210c93808d3771a28ac
Kallayne Silva Braga <129508875+kallayne@users.noreply.github.com>
2023-11-12 08:34:25 -0300

    Cartao- react

--- backend/API/src/user/repository/pagamentoRepository.js
+++ backend/API/src/user/repository/pagamentoRepository.js
@@ -106,3 +106,28 @@ export async function ListarCartaoCliente(idCliente){
 
     return verify[0]
 }
+
+export async function ListarCartaoClienteid(idCliente) {
+    let comando = `
+        SELECT 
+            cl.id_cliente     as idCliente,
+            ca.id_cartao      as idCartao,
+            ca.ds_numero      as num,
+            ca.ds_nome        as nome,
+            ca.ds_validade    as validade,  
+            ca.ds_cvv         as cvv
+        FROM tb_cliente cl
+        LEFT JOIN tb_cartao ca ON cl.id_cliente = ca.id_cliente
+        WHERE cl.id_cliente = ?
+    `;
+    
+    let [verify] = await con.query(comando, idCliente);
+
+    return verify; // Corrigir para retornar o array completo
+}
+
+
+
+
+
+
