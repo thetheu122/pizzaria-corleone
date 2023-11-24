093057858d665cc0be542cee6b3f5900505ae551
thetheu122 <math.santosmed@gmail.com>
2023-11-24 04:38:01 -0300

    pronto para ir para o servidor

--- frontend/src/components/perfil/historico/index.js
+++ frontend/src/components/perfil/historico/index.js
@@ -2,15 +2,10 @@ import './index.scss'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+import carregando from '../../../assets/images/carregando.png'
 import Cardhistorico from '../cardhistorico'
-<<<<<<< HEAD
 import { toast } from 'react-toastify'
-import axios from 'axios'
 import { API_URL } from '../../../config/constants'
-import { useEffect, useState } from 'react'
-=======
-import { API_URL } from '../../../config/constants'
->>>>>>> 3cb32f459974dc79f823633b923c01b1d566a787
 export default function Historico() {
     const [historico, setHistorico] = useState([])
 
@@ -43,11 +38,13 @@ listarcompras()
         <div className='fundo'>
             <h1>Historico de compras : </h1>
 
+            {historico.length > 0 ? 
             <div className='cartoesHistorico'>
                 {pedido.map((item)=>(
                     <Cardhistorico pedido={{id:item.idpedido, data:item.data,situacao:item.situacao,produtos:item.produtos}} />
                 ))}
             </div>
+            : <h1 className='pp'> <img src={carregando}/> Você ainda não efetuou nenhuma compra completamente</h1>}
         </div>
     )
 }
--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-//export const API_URL = 'http://129.148.42.252:5013'*/
+export const API_URL = 'http://129.148.42.252:5013'*/
 
-export const  API_URL = 'http://localhost:5013'
-=======
-export const API_URL = 'http://localhost:5013'
-
-// export const API_URL = 'http://129.148.42.252:5013'
->>>>>>> 3cb32f459974dc79f823633b923c01b1d566a787
+// export const  API_URL = 'http://localhost:5013'
